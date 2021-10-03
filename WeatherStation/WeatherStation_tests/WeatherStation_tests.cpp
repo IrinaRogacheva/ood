21a89a5c19ee895e4e35154bf5f67fb2589b0522
IrinaRogacheva <61589085+IrinaRogacheva@users.noreply.github.com>
2021-10-04 00:21:07 +0300

    добавила тест для 2 задания

---
+++ WeatherStation/WeatherStation_tests/WeatherStation_tests.cpp
@@ -0,0 +1,13 @@
+﻿#define CATCH_CONFIG_MAIN
+#include "../../catch2/catch.hpp"
+#include "../WeatherStation/WeatherData.h"
+
+SCENARIO("The observer removes itself from the list of observers from the Update method")
+{
+	CWeatherData wd;
+	
+	CObserverWhoRemovesItself observerWhoRemovesItself(wd);
+	wd.RegisterObserver(observerWhoRemovesItself);
+
+	wd.SetMeasurements(3, 0.7, 760);
+}
