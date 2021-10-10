a2efa7ff0c83a16fd6cdafedea71b665b5c6b7a4
IrinaRogacheva <61589085+IrinaRogacheva@users.noreply.github.com>
2021-10-10 23:42:14 +0300

    3 лабораторная

---
+++ decorator/coffee/Beverages.h
@@ -0,0 +1,137 @@
+﻿#pragma once
+
+#include "IBeverage.h"
+
+class CBeverage : public IBeverage
+{
+public:
+	CBeverage(const std::string & description)
+		:m_description(description)
+	{}
+
+	std::string GetDescription()const override final
+	{
+		return m_description;
+	}
+protected:
+	std::string m_description;
+};
+
+enum class CoffeeServingSize
+{
+	Standard,
+	Double,
+};
+
+class CCoffee : public CBeverage
+{
+public:
+	CCoffee(CoffeeServingSize size = CoffeeServingSize::Standard, const std::string& description = " coffee")
+		: CBeverage(description)
+		, m_servingSize(size)
+	{
+		m_description = (size == CoffeeServingSize::Standard ? "Standard" : "Double") + description;
+	}
+
+	double GetCost() const override 
+	{
+		return 60; 
+	}
+private:
+	CoffeeServingSize m_servingSize;
+};
+
+class CCappuccino : public CCoffee
+{
+public:
+	CCappuccino(CoffeeServingSize servingSize = CoffeeServingSize::Standard, const std::string& description = " cappuccino")
+		:CCoffee(servingSize, description)
+		, m_servingSize(servingSize)
+	{}
+
+	double GetCost() const override 
+	{
+		return (m_servingSize == CoffeeServingSize::Standard ? 80 : 120);
+	}
+private:
+	CoffeeServingSize m_servingSize;
+};
+
+class CLatte : public CCoffee
+{
+public:
+	CLatte(CoffeeServingSize servingSize = CoffeeServingSize::Standard, const std::string& description = " latte")
+		:CCoffee(servingSize, description)
+		, m_servingSize(servingSize)
+	{}
+
+	double GetCost() const override 
+	{
+		return (m_servingSize == CoffeeServingSize::Standard ? 90 : 130);
+	}
+private:
+	CoffeeServingSize m_servingSize;
+};
+
+enum class KindOfTea
+{
+	Black,
+	Green,
+	White,
+	Assam,
+};
+
+class CTea : public CBeverage
+{
+public:
+	CTea(KindOfTea kindOfTea = KindOfTea::Black, const std::string& description = " tea")
+		:CBeverage(description)
+	{
+		switch (kindOfTea)
+		{
+		case KindOfTea::Black:
+			m_description = "Black";
+			break;
+		case KindOfTea::Green:
+			m_description = "Green";
+			break;
+		case KindOfTea::White:
+			m_description = "White";
+			break;
+		case KindOfTea::Assam:
+			m_description = "Assam";
+			break;
+		}
+		m_description += description;
+	}
+
+	double GetCost() const override 
+	{
+		return 30; 
+	}
+};
+
+enum class MilkshakeServingSize
+{
+	Small,
+	Medium,
+	Big,
+};
+
+class CMilkshake : public CBeverage
+{
+public:
+	CMilkshake(MilkshakeServingSize servingSize = MilkshakeServingSize::Big, const std::string& description = " milkshake")
+		:CBeverage(description)
+		, m_servingSize(servingSize)
+	{
+		m_description = (servingSize == MilkshakeServingSize::Small ? "Small" : (servingSize == MilkshakeServingSize::Medium ? "Medium" : "Big")) + description;
+	}
+
+	double GetCost() const override 
+	{ 
+		return (m_servingSize == MilkshakeServingSize::Small ? 50 : (m_servingSize == MilkshakeServingSize::Medium ? 60 : 80));
+	}
+private:
+	MilkshakeServingSize m_servingSize;
+};
---
+++ decorator/coffee/main.cpp
@@ -0,0 +1,114 @@
+﻿#include "Beverages.h"
+#include "Condiments.h"
+
+#include <iostream>
+#include <string>
+#include <functional>
+
+using namespace std;
+
+struct MakeLemon
+{
+	MakeLemon(unsigned quantity)
+		:m_quantity(quantity)
+	{}
+
+	auto operator()(IBeveragePtr && beverage)const
+	{
+		return make_unique<CLemon>(move(beverage), m_quantity); 
+	}
+private:
+	unsigned m_quantity;
+};
+
+function<IBeveragePtr(IBeveragePtr &&)> MakeCinnamon()
+{
+	return [] (IBeveragePtr && b) {
+		return make_unique<CCinnamon>(move(b));
+	};
+}
+
+template <typename Condiment, typename... Args>
+auto MakeCondiment(const Args&...args)
+{
+	return [=](auto && b) {
+		return make_unique<Condiment>(forward<decltype(b)>(b), args...);
+	};
+}
+
+template <typename Component, typename Decorator>
+auto operator << (Component && component, const Decorator & decorate)
+{
+	return decorate(forward<Component>(component));
+}
+
+void DialogWithUser()
+{
+	cout << "Type 1 for coffee or 2 for tea\n";
+	int beverageChoice;
+	cin >> beverageChoice;
+
+	unique_ptr<IBeverage> beverage;
+
+	if (beverageChoice == 1)
+	{
+		beverage = make_unique<CCoffee>();
+	}
+	else if (beverageChoice == 2)
+	{
+		beverage = make_unique<CTea>();
+	}
+	else
+	{
+		return;
+	}
+
+	int condimentChoice;
+	for (;;)
+	{
+		cout << "1 - Lemon, 2 - Cinnamon, 0 - Checkout" << endl;
+		cin >> condimentChoice;
+
+		if (condimentChoice == 1)
+		{
+			beverage = move(beverage) << MakeCondiment<CLemon>(2);
+		}
+		else if (condimentChoice == 2)
+		{
+			beverage = move(beverage) << MakeCondiment<CCinnamon>();
+		}
+		else if (condimentChoice == 0)
+		{
+			break;
+		}
+		else
+		{
+			return;
+		}
+	}
+
+	cout << beverage->GetDescription() << ", cost: " << beverage->GetCost() << endl;
+}
+
+int main()
+{
+	auto latte = make_unique<CLatte>(CoffeeServingSize::Double);
+	auto cinnamon = make_unique<CCinnamon>(move(latte));
+	auto lemon = make_unique<CLemon>(move(cinnamon), 2);
+	auto iceCubes = make_unique<CIceCubes>(move(lemon), 2, IceCubeType::Dry);
+	auto cream = make_unique<CCream>(move(iceCubes));
+	auto chocolate = make_unique<CChocolate>(move(cream), 6);
+	auto liquor = make_unique<CLiquor>(move(chocolate), LiquorType::Nutty);
+	auto beverage = make_unique<CChocolateCrumbs>(move(liquor), 2);
+
+	auto cappuccino = make_unique<CCappuccino>(CoffeeServingSize::Double);
+
+	auto tea = make_unique<CTea>(KindOfTea::Assam);
+
+	auto milkshake = make_unique<CMilkshake>(MilkshakeServingSize::Small);
+
+	cout << beverage->GetDescription() << " costs " << beverage->GetCost() << endl;
+	cout << cappuccino->GetDescription() << " costs " << cappuccino->GetCost() << endl;
+	cout << tea->GetDescription() << " costs " << tea->GetCost() << endl;
+	cout << milkshake->GetDescription() << " costs " << milkshake->GetCost() << endl;
+}
